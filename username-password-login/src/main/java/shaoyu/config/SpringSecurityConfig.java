e2e397e9294387ad5f6b5935161e40a37b74985c
leesaoyu <1663423581@qq.com>
2019-11-01 10:44:42 +0800

    1.图形验证码-可以用了，异常返回信息未处理 2.短信验证码-无法使用

--- username-password-login/src/main/java/shaoyu/config/SpringSecurityConfig.java
+++ username-password-login/src/main/java/shaoyu/config/SpringSecurityConfig.java
@@ -1,14 +1,24 @@
 package shaoyu.config;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.web.bind.annotation.PutMapping;
+import shaoyu.validate.SmsCodeFilter;
+import shaoyu.validate.ValidateCodeFilter;
+
+import javax.servlet.Filter;
+import javax.sql.DataSource;
 
 /**
  * @author leeshaoyu
@@ -31,9 +41,23 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private MyLogoutSuccessHandler LogoutSuccessHandler;
 
+	@Autowired
+	private HikariDataSource hikariDataSource;
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public PersistentTokenRepository persistentTokenRepository() {
+		JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+		// tokenRepository.setCreateTableOnStartup(true);
+		tokenRepository.setDataSource(hikariDataSource);
+		return tokenRepository;
 	}
 
 	@Override
@@ -44,7 +68,17 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.formLogin()
+
+		ValidateCodeFilter validateCodeFilter = new ValidateCodeFilter();
+		validateCodeFilter.setAuthenticationFailureHandler(failureHandler);
+
+		SmsCodeFilter smsCodeFilter = new SmsCodeFilter();
+		smsCodeFilter.setAuthenticationFailureHandler(failureHandler);
+
+		http
+				.addFilterBefore(validateCodeFilter, UsernamePasswordAuthenticationFilter.class)
+				// .addFilterAfter(smsCodeFilter, UsernamePasswordAuthenticationFilter.class)
+				.formLogin()
 				//  允许为与基于表单的登录相关联的所有URL授予对所有用户的访问权限。
 				.permitAll()
 				//  自定义登录页面
@@ -55,15 +89,11 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 				.successHandler(successHandler)
 				//  登录失败处理
 				.failureHandler(failureHandler)
-				// .and()
-				// .rememberMe()
-				// .userDetailsService(userDetailService)
-				// .tokenRepository()
 				.and()
 				//  身份认证
 				.authorizeRequests()
 				//  添加多个子项来指定网址的自定义要求，表示访问 /admin/** 这个接口，需要具备 admin 这个角色
-				.antMatchers("/login").permitAll()
+				.antMatchers("/login", "/code/image").permitAll()
 				.antMatchers("/admin/**").hasRole("admin")
 				.antMatchers("/access/**").access("hasRole('ADMIN') and hasRole('MANAGER')")
 				.anyRequest()
@@ -73,6 +103,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 				.and()
 				.csrf().disable();
 
+		// 注销
 		http.logout()
 				//  注销后重定向到的URL
 				.logoutUrl("/logout")
@@ -81,6 +112,10 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 				.logoutSuccessHandler(LogoutSuccessHandler)
 				//  指定在注销成功时删除的cookie的名称
 				.deleteCookies();
+		//  记住密码
+		http.rememberMe()
+				.userDetailsService(userDetailService)
+				.tokenRepository(persistentTokenRepository());
 	}
 
 
